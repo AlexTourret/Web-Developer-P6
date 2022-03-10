41a296a802bc574a74eb02b0eadcd499f5923507
Alex Tourret <alexandretourret@ciblemut.net>
2022-03-10 15:16:09 +0100

    maj sauce get delete

--- controllers/sauce.js
+++ controllers/sauce.js
@@ -1,1 +1,43 @@
 const Sauce = require('../models/Sauce');
+
+exports.getOneSauce = (req, res, next) => {
+  Sauce.findOne({ _id: req.params.id })
+    .then(sauce => res.status(200).json(sauce))
+    .catch(error => res.status(404).json({ error }));
+};
+
+exports.getAllSauce = (req, res, next) => {
+    Sauce.find()
+    .then(sauces => res.status(200).json(sauces))
+    .catch(error => res.status(400).json({ error }));
+};
+
+exports.deleteSauce = (req, res, next) => {
+  Sauce.findOne({ _id: req.params.id }).then(
+    (sauce) => {
+      if (!sauce) {
+        res.status(404).json({
+          error: new Error('Sauce non trouvée!')
+        });
+      }
+      if (sauce.userId !== req.auth.userId) {
+        res.status(400).json({
+          error: new Error('Unauthorized request!')
+        });
+      }
+      Sauce.deleteOne({ _id: req.params.id }).then(
+        () => {
+          res.status(200).json({
+            message: 'Deleted!'
+          });
+        }
+      ).catch(
+        (error) => {
+          res.status(400).json({
+            error: error
+          });
+        }
+      );
+    }
+  )
+};
