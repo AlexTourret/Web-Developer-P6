ced3b27e535a3abd0ece3cf7130503c80dfeca95
Alex Tourret <alexandretourret@ciblemut.net>
2022-03-10 15:01:37 +0100

    init route

---
+++ routes/sauce.js
@@ -0,0 +1,7 @@
+const express = require('express');
+const auth = require('../middleware/auth');
+const sauceCtrl = require('../controllers/sauce');
+const router = express.Router();
+
+
+module.exports = router;
