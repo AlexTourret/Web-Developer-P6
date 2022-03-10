91758be3c962123cbfc65a71625b40b8ce9cc74d
Alex Tourret <alexandretourret@ciblemut.net>
2022-03-10 15:16:38 +0100

    maj sauce get delete

--- routes/sauce.js
+++ routes/sauce.js
@@ -3,5 +3,10 @@ const auth = require('../middleware/auth');
 const sauceCtrl = require('../controllers/sauce');
 const router = express.Router();
 
+router.get('/:id',auth, sauceCtrl.getOneSauce);
+
+router.get('/',auth, sauceCtrl.getAllSauce);
+
+router.delete('/:id',auth, sauceCtrl.deleteSauce);
 
 module.exports = router;
