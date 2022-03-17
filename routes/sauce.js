6b548a204674245ba57bdd6fab28330a088fca07
Alex Tourret <alexandretourret@ciblemut.net>
2022-03-17 13:53:14 +0100

    ajout du dotenv + create et modify sauce

--- routes/sauce.js
+++ routes/sauce.js
@@ -2,11 +2,16 @@ const express = require('express');
 const auth = require('../middleware/auth');
 const sauceCtrl = require('../controllers/sauce');
 const router = express.Router();
+const multer = require('../middleware/multer-config');
 
 router.get('/:id',auth, sauceCtrl.getOneSauce);
 
 router.get('/',auth, sauceCtrl.getAllSauce);
 
 router.delete('/:id',auth, sauceCtrl.deleteSauce);
+
+router.post('/', auth, multer, sauceCtrl.createSauce);
+
+router.put('/:id', auth, multer, sauceCtrl.modifySauce);
 
 module.exports = router;
