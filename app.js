a90977d9f312fd48925e020d2faef4dff4e69d78
Alex Tourret <alexandretourret@ciblemut.net>
2022-03-24 10:29:35 +0100

    finalisation du code

--- app.js
+++ app.js
@@ -14,7 +14,9 @@ mongoose.connect (process.env.DATABASE_MONGO,
   .catch(() => console.log('Connexion � MongoDB �chou�e !'));
 
 app.use(express.json());
-
+//Permet de faire d'accéder à l'api depuis n'importe quelle origine ('*')
+// ajouter les headers mentionnés aux requêtes envoyées vers notre API
+// envoyer des requêtes avec les méthodes mentionnées
 app.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
@@ -24,6 +26,6 @@ app.use((req, res, next) => {
 
 app.use("/images", express.static(path.join(__dirname, 'images')));
 app.use('/api/auth',userRoutes);
-app.use('/api/sauce',sauceRoutes);
+app.use('/api/sauces',sauceRoutes);
 
 module.exports = app;
