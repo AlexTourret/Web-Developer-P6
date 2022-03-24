492b7c4eb9a488d717b27fbdc27ccadb3b1b9663
Alex Tourret <alexandretourret@ciblemut.net>
2022-03-24 10:30:27 +0100

    finalisation du code

--- routes/sauce.js
+++ routes/sauce.js
@@ -6,12 +6,15 @@ const multer = require('../middleware/multer-config');
 
 router.get('/:id',auth, sauceCtrl.getOneSauce);
 
-router.get('/',auth, sauceCtrl.getAllSauce);
+router.get('/', sauceCtrl.getAllSauce);
 
 router.delete('/:id',auth, sauceCtrl.deleteSauce);
 
 router.post('/', auth, multer, sauceCtrl.createSauce);
 
 router.put('/:id', auth, multer, sauceCtrl.modifySauce);
+
+router.post('/:id/like', auth, sauceCtrl.likeSauce);
+
 
 module.exports = router;
